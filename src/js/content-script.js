72e7f8d94e307b2fe1c0875588c12afd25af32c7
lukewiwa <luke.wiwa@gmail.com>
2018-02-01 19:52:56 +1100

    eslint cleaning

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -1,26 +1,25 @@
 import '../scss/content-script.scss'
 
-const search_terms = ['communal']
+const searchTerms = ['communal']
 
 const highlight = (terms) => {
-    let paragraphs = document.getElementsByClassName("body")[0]
-    let extra = paragraphs.getElementsByTagName('span')[0]
-    let body
-    if (extra) {
-        let link = paragraphs.getElementsByClassName('more interesting')[0]
-        link.remove()
-        extra.remove()
-        body = paragraphs.innerHTML + extra.getAttribute("data-description")
-
-    }else{
-        body = paragraphs.innerHTML
-    }
-    terms.forEach(term => {
-        let searchTerm = new RegExp("(" + term + ")", "gi") 
-        console.log(searchTerm)
-        body = body.replace(searchTerm, '<span class="highlight">$1</span>')
-    })
-    paragraphs.innerHTML = body
+  let paragraphs = document.getElementsByClassName('body')[0]
+  let extra = paragraphs.getElementsByTagName('span')[0]
+  let body
+  if (extra) {
+    let link = paragraphs.getElementsByClassName('more interesting')[0]
+    link.remove()
+    extra.remove()
+    body = paragraphs.innerHTML + extra.getAttribute('data-description')
+  } else {
+    body = paragraphs.innerHTML
+  }
+  terms.forEach(term => {
+    let searchTerm = new RegExp('(' + term + ')', 'gi')
+    console.log(searchTerm)
+    body = body.replace(searchTerm, '<span class="highlight">$1</span>')
+  })
+  paragraphs.innerHTML = body
 }
 
-window.onload = highlight(search_terms)
+window.onload = highlight(searchTerms)
