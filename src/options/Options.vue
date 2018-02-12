6841e4ff0fcd8891e54b085bfd4ad834f0752346
lukewiwa <luke.wiwa@gmail.com>
2018-02-12 20:58:13 +1100

    bootstrap refinements

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -6,19 +6,22 @@
       class="input-group"
       :key="index" 
       v-for="(term, index) in terms">
-        <input class="form-control" v-model="term.word"/>
+        <input class="form-control" v-model="term.word" required>
         <div class="input-group-append">
           <button 
           v-on:click="add(index)"
           type="button"
-          class="btn btn-success">+</button>
+          class="btn btn-outline-success">+</button>
           <button v-on:click="remove(index)"
           type="button"
-          class="btn btn-danger">-</button>
+          class="btn btn-outline-danger">-</button>
         </div>
       </li>
     </ul>
     <button type="button" class="btn btn-primary" v-on:click="save()">Save</button>
+    <div class="alert alert-primary" role="alert" v-if="alert">
+      {{alert}}
+    </div>
 	</div>
 </template>
 
@@ -26,16 +29,25 @@
 const storageArea = chrome.storage.sync
 
 export default {
-  name: "Options",
+  name: 'Options',
   data () {
     return {
       empty: {'word': ''},
-      terms: [Object.assign({}, this.empty)]
+      terms: [Object.assign({}, this.empty)],
+      alert: false
     }
   },
   methods: {
     save: function () {
-      storageArea.set({ terms: this.terms })
+      storageArea.set(
+        { terms: this.terms },
+        () => {
+          this.alert = 'saved succesfully!',
+          setTimeout(() => {
+            this.alert = false;
+        }, 2000);
+        }
+      )
     },
     add: function (index) {
       this.terms.splice(index + 1, 0, Object.assign({}, this.empty))
@@ -61,9 +73,13 @@ ul {
   list-style: none;
   padding-left: 0;
 }
+li {
+  margin-top: 0.5em;
+}
 #options {
   margin: 2em;
   min-width: 30em;
+  min-height: 25em;
 }
 </style>
 
